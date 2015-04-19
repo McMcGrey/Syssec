434f62059c2256572c75405f0fd5d5890d871d72
Arne <arnemarijan.kohlbacher@aau.at>
2015-04-19 23:52:58 +0200

    Update SigTest.java

--- SS_Ab2/src/at/syssec/ss15/ss/ab2/test/SigTest.java
+++ SS_Ab2/src/at/syssec/ss15/ss/ab2/test/SigTest.java
@@ -1,7 +1,33 @@
 package at.syssec.ss15.ss.ab2.test;
 
 
+import at.syssec.ss15.ss.ab2.ElGamalSig;
+import at.syssec.ss15.ss.ab2.impl.kohlbacher_wutti.ElGamalSigImpl;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigInteger;
+
 public class SigTest {
 
-	//Folgt
+    ElGamalSig tools = new ElGamalSigImpl();
+
+    @Test
+    public void testSignatur() {
+
+        byte[] message = "Das ist ein SysSec-Test".getBytes();
+
+        BigInteger p = tools.generatePrime(20);
+
+        BigInteger g = tools.generateGenerator(p);
+
+        BigInteger d = tools.generatePrivatePart(p);
+
+        BigInteger e = tools.generatePublicPart(p, g, d);
+
+        ElGamalSig.ElGamalSignature sig = tools.sign(message, p, g, d);
+
+        Assert.assertTrue(tools.verify(message, sig, p, g, e));
+    }
+
 }
